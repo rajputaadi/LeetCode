e6189ebdf52af9bc8aaa1fc5a25946395aa42276
rajputaadi <139755855+rajputaadi@users.noreply.github.com>
2024-09-13 11:17:42 +0530

    Time: 143 ms (47.58%), Space: 75 MB (29.44%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,46 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-
     ListNode* FindMiddle(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(fast!=NULL&& fast->next!=NULL){
-            slow = slow->next;
-            fast= fast->next->next;
+        ListNode* low = head;
+        ListNode* high = head->next;
+        while( high !=NULL && high->next != NULL){
+            low = low->next;
+            high = high->next->next;
         }
-        return slow;
+        return low;
     }
 
-    ListNode* MergeLists(ListNode* list1,ListNode* list2){
-        ListNode* DummyNode = new ListNode(-1);
-        ListNode* temp = DummyNode;
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val<list2->val){
+    ListNode* mergeTwoSortList(ListNode* list1, ListNode* list2){
+        ListNode* dummynode = new ListNode(-1);
+        ListNode* temp = dummynode;
+
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val < list2->val){
                 temp->next = list1;
                 temp = list1;
-                list1=list1->next;
+                list1 = list1->next;
             }
             else{
-                temp->next = list2;          
+                temp->next = list2;
                 temp = list2;
-                list2=list2->next;
+                list2 = list2->next; 
             }
         }
         if(list1) temp->next = list1;
         else temp->next = list2;
-        return DummyNode->next;
+        return dummynode->next;
     }
 
     ListNode* sortList(ListNode* head) {
-        if(head==NULL|| head->next==NULL) return head;
+        if(head==NULL || head->next==NULL) return head;
+
         ListNode* middle = FindMiddle(head);
         ListNode* right = middle->next;
-        middle->next = nullptr;
+        middle->next = NULL;
         ListNode* left = head;
 
         left = sortList(left);
         right = sortList(right);
-        return MergeLists(left,right);
+        return mergeTwoSortList(left,right);
     }
-
 };
